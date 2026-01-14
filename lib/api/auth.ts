caa625063a245e748dca23c3eea93c5b48bd8ba7
Nipuana <nipuanarimal@gmail.com>
2026-01-14 07:42:52 +0545

    added register logic and error handlinng for accessing the path and calling the register function

---
+++ lib/api/auth.ts
@@ -0,0 +1,21 @@
+// API layer
+// Call api from backend
+
+import axios from "axios";
+import { API } from "./endpoints";
+
+export const register = async ( registerData : any ) => {
+    try{
+        const response = await axios.post(
+            API.AUTH.REGISTER, //path
+            registerData //body data
+        );
+        return response.data;
+    } catch (err: Error | any) {
+        throw new Error(
+            err.response?.data?.message // log error message from backend
+             || err.message // default error message
+             || "Registration failed" //fallback message if default fails
+        );
+    }
+}
