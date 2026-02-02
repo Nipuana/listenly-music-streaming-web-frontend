0a2ca98a22e379ea06ae253d2ddaf52c9833765d
Nipuana <nipuanarimal@gmail.com>
2026-02-02 19:43:06 +0545

    updated endpoints

--- lib/api/endpoints.ts
+++ lib/api/endpoints.ts
@@ -4,5 +4,16 @@
 export const API = {
  AUTH:{
     REGISTER: 'api/auth/register',
-    LOGIN: 'api/auth/login',}
+    LOGIN: 'api/auth/login',
+    GETPROFILE: 'api/auth/profile',
+    UPDATEPROFILE:'api/auth/update-profile'
+},
+
+AD_USERS: {
+    CREATE_USER: 'api/admin/create-user',
+    GET_ALL_USERS: 'api/admin/get-all-users',
+    GET_USER: 'api/admin/get-user',
+    UPDATE_USER: 'api/admin/update-user',
+    DELETE_USER: 'api/admin/delete-user'
+}
 };
