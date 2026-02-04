cb71079de9fd321317ccd3f6b956b46d75bb2947
Nipuana <nipuanarimal@gmail.com>
2026-02-04 19:53:54 +0545

    added USER_INFO endpoints

--- lib/api/endpoints.ts
+++ lib/api/endpoints.ts
@@ -2,20 +2,28 @@
 // Single source of truth for api endpoints
 
 export const API = {
- AUTH:{
-    REGISTER: 'api/auth/register',
-    LOGIN: 'api/auth/login',
-    GETPROFILE: 'api/auth/profile',
-    UPDATEPROFILE:'api/auth/update-profile',
+  AUTH: {
+    REGISTER: '/api/auth/register',
+    LOGIN: '/api/auth/login',
+    GET_PROFILE: '/api/auth/profile',
+    UPDATE_PROFILE: '/api/auth/update-profile',
     REQUEST_PASSWORD_RESET: '/api/auth/request-password-reset',
     RESET_PASSWORD: (token: string) => `/api/auth/reset-password/${token}`,
-},
+  },
 
-AD_USERS: {
-    CREATE_USER: 'api/admin/create-user',
-    GET_ALL_USERS: 'api/admin/get-all-users',
-    GET_USER: 'api/admin/get-user',
-    UPDATE_USER: 'api/admin/update-user',
-    DELETE_USER: 'api/admin/delete-user'
-}
+  AD_USERS: {
+    CREATE_USER: '/api/admin/create-user',
+    GET_ALL_USERS: '/api/admin/get-all-users',
+    GET_USER: (id: string) => `/api/admin/get-user/${id}`,
+    UPDATE_USER: (id: string) => `/api/admin/update-user/${id}`,
+    DELETE_USER: (id: string) => `/api/admin/delete-user/${id}`,
+  },
+
+  USER_INFO: {
+    ADD_INFO: '/api/userInfo/add-info',
+    GET_INFO: '/api/userInfo/get-info',
+    CLEAR_INFO: '/api/userInfo/clear-info',
+  },
+
+  
 };
