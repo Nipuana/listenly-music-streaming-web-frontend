dcdc54f39b78fae6e66fc7dc72d082a766a1e103
Nipuana <nipuanarimal@gmail.com>
2026-02-28 00:02:01 +0545

    added sidebar to dashboard

--- app/user/dashboard/page.tsx
+++ app/user/dashboard/page.tsx
@@ -1,11 +1,19 @@
+"use client";
+
 import Header from "@/components/layout/header";
 import { DashboardClient } from "./_components/DashboardClient";
+import { SidebarProvider } from "@/Providers/Contexts/SidebarContext";
+import { useAuth } from "@/Providers/Contexts/auth-context";
 
 export default function DashboardPage() {
+  
+  const { user, logout } = useAuth();
   return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <DashboardClient />
-    </div>
+    <SidebarProvider user={user}>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <DashboardClient />
+      </div>
+    </SidebarProvider>
   );
 }
